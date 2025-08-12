ea3cb238f3e7ccdede27befb5b2c287049d0d70e
ValeriaAc <valee5acatitla@gmail.com>
2025-08-12 12:58:06 -0600

    Se agregaron cambios al footer

---
+++ JS/footer.js
@@ -0,0 +1,6 @@
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer-container").innerHTML = data;
+  })
+  .catch(error => console.error("Error al cargar el footer:", error));
--- JS/nav.js
+++ JS/nav.js
@@ -5,3 +5,5 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("navbar-placeholder").innerHTML = data;
     });
 });
+
+
